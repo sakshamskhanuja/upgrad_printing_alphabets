2c09d985be01d38a9fdc9033d117567d9925335d
Saksham Singh Khanuja <sskhanuja94@gmail.com>
2023-01-09 09:09:48 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int start = 97;
+        while (start <= (97 + 25)) {
+            System.out.print((char) start + " ");
+            start++;
+        }
+    }
+}
